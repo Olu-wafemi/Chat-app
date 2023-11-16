7393e75fa76fcb9eed19a6c3a506a88de2a59ead
Oluwafemi <femio82@gmail.com>
2023-11-16 15:46:54 +0100

    added:message-service

---
+++ src/messages/message.service.ts
@@ -0,0 +1,19 @@
+import {Injectable } from "@nestjs/common"
+
+import { InjectRepository } from "@nestjs/typeorm"
+
+import { Repository } from "typeorm"
+
+import { Message  } from "./message.entity"
+@Injectable()
+export class MessageService{
+    constructor(
+    @InjectRepository(Message)
+    private readonly messageRepository: Repository<Message>, 
+    ) {}
+
+    async findAll(): Promise<Message[]>{
+        return await this.messageRepository.find();
+    }
+
+}
