7c32b8a8245397ec9bb7bdf1a58ff1373ba67520
Oluwafemi <femio82@gmail.com>
2023-11-16 16:17:57 +0100

    added:user_entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,20 @@
+import { Message } from '../messages/message.entity';
+import { Entity, Column, PrimaryGeneratedColumn
+,OneToMany } from "typeorm";
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique: true})
+    username: string;
+
+    //A one-to-many relationship with the room entitiy, indicating that a user can own many rooms
+    @OneToMany(()=> Room, (room)=> room.owner)
+    ownedRooms: Room[];
+
+    //a one-to-many relatonship with the message entity, indicatiing that a user can send multiole messages
+    @OneToMany(()=> Message, (message)=> message.user)
+    messages: Message[]
+}
