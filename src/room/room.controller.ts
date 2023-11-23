8b53477cc87627c5e81a8b814ee99a7b29a85840
Oluwafemi <femio82@gmail.com>
2023-11-23 13:32:47 +0100

    added:room_controller

---
+++ src/room/room.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { MessagesController } from '../messages/message.controller';
+import { MessageService } from '../messages/message.service';
+import { Message } from '../messages/message.entity';
+
+@Controller('messages')
+export class MessageController {
+    constructor(private readonly messageService: MessageService){}
+
+    @Get()
+    findAll(): Promise<Message[]>{
+        return this.messageService.findAll();
+    }
+    @Get('id')
+    findbyId(@Param('id')id: string): Promise<Message| undefined>{
+        return this.messageService.findById(Number(id))
+
+    }
+
+    @Get('room/:roomId')
+    findByRoom(@Param('roomId') roomId: string): Promise<Message[]>{
+        return this.messageService.findByRoom(Number(roomId));
+    }
+    @Post()
+    create(@Body() message: Partial<Message>): Promise<Message>{
+        return this.messageService.create(message)
+    }
+    @Delete(':id')
+    delete(@Param('id') id: string): Promise<void>{
+        return this.messageService.delete(Number(id))
+    }
+
+}
